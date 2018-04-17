0f35710ecde639c4bb70744f4b638354a019dae4
GiriBaldySyachbanny <37504833+GiriBaldySyachbanny@users.noreply.github.com>
2018-04-17 20:06:57 +0700

    Add files via upload

---
+++ UAS.py
@@ -0,0 +1,15 @@
+import GiriBaldy.dictgiri
+import GiriBaldy.listgiri
+import GiriBaldy.nilaimahasiswagiri
+import GiriBaldy.RANDOMgiri
+import GiriBaldy.TUPLEgiri
+
+print (GiriBaldy.listgiri.ga())
+
+print (GiriBaldy.listgiri.gb())
+
+print (GiriBaldy.nilaimahasiswagiri.gc())
+
+print (GiriBaldy.RANDOMgiri.gd())
+
+print (GiriBaldy.TUPLEgiri.ge())
